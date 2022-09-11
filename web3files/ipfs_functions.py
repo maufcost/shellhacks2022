936e258ad4fee17a0bdfe170252df0c0fffb8a9a
Nate8888 <wilk.nathan@gmail.com>
2022-09-11 02:22:59 -0300

    Add docstrings, fix vars & add ipfs code parse

--- web3files/ipfs_functions.py
+++ web3files/ipfs_functions.py
@@ -16,7 +16,22 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 dotenv.load_dotenv()
 IPFS_KEY = os.getenv("IPFS_KEY", None)
 
-def parse_ipfs_rule_details(url_for_rule: str) -> dict:
+'''
+Takes the url to an ipfs code object and return the entire code
+'''
+def parse_ipfs_code_from(code: str) -> str:
+    if not code.startswith("ipfs://"):
+        raise Exception("Invalid IPFS URL")
+    code = code.replace("ipfs://", "https://ipfs.io/ipfs/")
+    response = requests.get(code)
+    if response.status_code != 200:
+        raise Exception(response.text)
+    return response.text
+
+'''
+Takes the url to an ipfs rule object and return the entire regex rule
+'''
+def parse_ipfs_rule_details(url_for_rule: str) -> str:
     if url_for_rule.startswith("ipfs://"):
         url_for_rule = url_for_rule.replace("ipfs://", "https://ipfs.io/ipfs/")
     if not url_for_rule.startswith("https://ipfs.io/ipfs/"):
@@ -26,6 +41,9 @@ def parse_ipfs_rule_details(url_for_rule: str) -> dict:
         raise Exception(response.text)
     return response.text
 
+'''
+Takes the url to an ipfs metadata object and return the entire metadata
+'''
 def parse_ipfs_url_data(url: str) -> dict:
     if url.startswith("ipfs://"):
         url = url.replace("ipfs://", "https://ipfs.io/ipfs/")
@@ -34,6 +52,10 @@ def parse_ipfs_url_data(url: str) -> dict:
     response = requests.get(url).json()
     return response
 
+'''
+Takes the url to an ipfs metadata object, gets the rule from, and returns the entire metadata
+including the regex rule
+'''
 def parse_all_ipfs_data(metadata_url: str) -> dict:
     metadata = parse_ipfs_url_data(metadata_url)
     rule = parse_ipfs_rule_details((metadata["rule"])).replace("\r", "")    
@@ -41,10 +63,16 @@ def parse_all_ipfs_data(metadata_url: str) -> dict:
     metadata["rule"] = rule
     return metadata
 
+'''
+Parses .rg file and splits it into the rule and info sections
+'''
 def parse_rule_file(contents: str) -> list([str, str]):
     res = re.search(r'% Rule\n+(.*)\n+% Info\n+([\s+\S+]+)', str(contents).replace("\r", ""))
     return [res.group(1), res.group(2)]
 
+'''
+Makes sure valid regex is uploaded so website doesn't break
+'''
 def check_regex_validity(regex: str) -> bool:
     try:
         re.compile(regex)
@@ -52,6 +80,10 @@ def check_regex_validity(regex: str) -> bool:
         return False
     return True
 
+'''
+Parses .rg file from local directory or from web and uploads to IPFS
+after checking if it's valid and safe.
+'''
 def parse_rg_file(rg_file: str, web: bool) -> list([str, str]):
     rule, info = ["", ""]
     if not web and (not rg_file or not rg_file.endswith(".rg")):
@@ -71,6 +103,10 @@ def parse_rg_file(rg_file: str, web: bool) -> list([str, str]):
     return rule, info
 
 # {'ok': True, 'value': {'ipnft': 'bafyreigjo7fx4aue7uxsnytuaavpznrpgvsbkdksppznne6gsnjf5bam34', 'url': 'ipfs://bafyreigjo7fx4aue7uxsnytuaavpznrpgvsbkdksppznne6gsnjf5bam34/metadata.json', 'data': {'name': 'test-rule-nobreak.rg', 'rule': 'ipfs://bafybeiewemksdxscjv7olvpw6bab2tycprgbpevvfctlwgfiyit3kmtsty/test-rule-nobreak.rg', 'info': ['https://cwe.mitre.org/data/definitions/94.html']}}} 
+'''
+Uploads a security regex rule to IPFS if all conditions are satisfied.
+Returns the dictionary response from IPFS.
+'''
 def upload_rule_to_ipfs(rg_file: str, obj_metadata: dict, web: bool) -> dict:
     fields = [] # options for us: "name", "description", "tags", "category", "image", "image_url"
     if IPFS_KEY is None:
@@ -106,6 +142,9 @@ def upload_rule_to_ipfs(rg_file: str, obj_metadata: dict, web: bool) -> dict:
     except Exception as e:
         raise e
 
+'''
+Upload a code file to ipfs and return the ipfs data.
+'''
 def upload_code_to_ipfs(code_bytes: bytes, obj_metadata: dict) -> dict:
     fields = [] # options for us: "name", "description", "tags", "category", "image", "image_url"
     if IPFS_KEY is None:
@@ -133,6 +172,7 @@ def upload_code_to_ipfs(code_bytes: bytes, obj_metadata: dict) -> dict:
     except Exception as e:
         raise e
 
+# Route to upload a regex rule to IPFS
 @cross_origin()
 @app.route("/upload-rule", methods=["POST"])
 def upload_rule():
@@ -144,6 +184,7 @@ def upload_rule():
 
     return upload_rule_to_ipfs(file_from_form.read(), {"name": name, "rule":"undefined"}, True)
 
+# Route to upload code files to IPFS
 @cross_origin()
 @app.route("/upload-code", methods=["POST"])
 def upload_code():
